d0f8074204b9f6ed34b7d86492c2bab9fac20639
Marcos Milhomem <marcosapolo511@gmail.com>
2023-07-10 13:57:12 -0300

    x-mem

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,14 @@ const personagens = document.querySelectorAll ('.personagem');
 
 personagens.forEach ((personagem) => {
     personagem.addEventListener('mouseenter', () => {
-        personagem.classList.add('selecionado')
+
         const personagemSelecionado = document.querySelector ('.selecionado')
         personagemSelecionado.classList.remove('selecionado')
 
         personagem.classList.add('selecionado')
 
         const imagemPersonagemGrande = document.querySelector('.personagem-grande')
-        imagemPersonagemGrande.src = ".src/imagens/card-jean-gray.png";
+        
+        imagemPersonagemGrande.src = "../imagens/card-vampira.png";
     })
 })
